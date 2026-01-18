8a80a1d458a232e61a531f65582614828c6acfdf
Shyam-AI-Engineer <shyamgenaiengineer@gmail.com>
2026-01-18 16:28:16 +0530

    single document chat code added

--- src/singledocument_chat/data_ingestion.py
+++ src/singledocument_chat/data_ingestion.py
@@ -4,3 +4,29 @@ import sys
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class SingleDocIngestor:
+    def__init__(self):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+        except Exception as e:
+            self.log.error("Failed to initialize SingleDocIngestor", error=str(e))
+            raise DocumentPortalException("Initialization error in SingleDocIngestor", sys)
+
+    def ingest_files(self):
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Document ingestion failed", error=str(e))
+            raise DocumentPortalException("Error during file ingestion", sys)
+    
+    def _create_retriever(self):
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Retriever creation failed", error=str(e))
+            raise DocumentPortalException("Error creating FAISS retriever", sys)
+                        
+    
---
+++ src/singledocument_chat/retrieval.py
@@ -0,0 +1,4 @@
+import sys
+import os
+from dotenv import load_dotenv
+from langchain_core.chat_history import BaseChatMessageHistory
